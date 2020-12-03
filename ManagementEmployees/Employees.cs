edbd1a322675e884b2b226b62c167eae286a8cda
BacDay32 <lephuocbac@gmail.com>
2020-12-03 18:49:45 +0700

    commit first

---
+++ ManagementEmployees/Employees.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementEmployees
+{
+    class Employees
+    {
+        public string idEmployees { get; set; }
+        public string fullName { get; set; }
+        public string dateOfBirth { get; set; }
+        public string address { get; set; }
+        public string position { get; set; }
+        public int salary { get; set; }
+        public Employees(string idEmployees, string fullName, string dateOfBirth, string position, int salary)
+        {
+            this.idEmployees = idEmployees;
+            this.fullName = fullName;
+            this.dateOfBirth = dateOfBirth;
+            this.address = address;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public Employees()
+        {
+        }
+
+        public void Add(string idEmployees, string fullName, string dateOfBirth,string address ,string position, int salary)
+        {
+            this.idEmployees = idEmployees;
+            Console.WriteLine("Enter full name employees :");
+            this.fullName = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter date of birthday employees :");
+            this.dateOfBirth = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter position employees :");
+            this.address = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter address employees :");
+            this.address = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter address employees :");
+
+            this.position = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter salary employees :");
+            this.salary = int.Parse(Console.ReadLine());
+        }
+        public void Remove(string idEmployees)
+        {
+
+        }
+        public void Show()
+        {
+            
+        }
+
+
+    }
+}
---
+++ ManagementEmployees/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ManagementEmployees;
+
+namespace ManagementEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var reader = new StreamReader(File.OpenRead(@"C:\InforEmployee.csv"));
+            var list = new List<Employees>();
+            var filePath = File.ReadAllLines(@"C:\Users\Desktop-win-bac\Documents\Visual Studio 2015\Projects\ManagementEmployees\InforEmployee.csv");
+            int count = 0;
+            foreach(var line in filePath)
+            {   
+                if(count == 0)
+                {
+                    count++;
+                    continue;
+                }
+
+                var value = line.Split(','); 
+                Employees employees = new Employees { idEmployees = value[0], fullName = value[1],  dateOfBirth= value[2], position= value[3], salary = int.Parse(value[4]) };
+                list.Add(employees);
+                Console.WriteLine(line);
+            }
+
+           
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
